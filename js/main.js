c3b0bef80966521244d1ad774f2ab09e42a1a4a1
max <max.saraev.web@gmail.com>
2023-10-08 20:02:31 +0300

    module_02--lesson_02: raw draft

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const overlay = document.querySelector('.overlay');
+
+const modalTitle = overlay.querySelector('.modal__title');
+
+const modalForm = overlay.querySelector('.modal__form');
+
+const modalCheckBox = overlay.querySelector('.modal__checkbox');
+
+const modalInputDiscount = overlay.querySelector('.modal__input_discount');
